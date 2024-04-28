79afd34b0328daf63182b73d73a4c65e5b68871c
Geert linders <geert.linders@salcom.com>
2024-04-28 20:59:51 +1200

    added support for meida files and added profile pic for users
    Change-Id: Ia764b8eb987fa83ed30445c9092599b900300716

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+profile_pics_dir = 'profile_pics'
+default_profile_pic = os.path.join(profile_pics_dir, 'default.png')
+
+
+class Profile(models.Model):
+    # connect out user model to django's user model
+    # on delete, delete everything i.e. cascade
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # extend the model with a profile picture
+    image = models.ImageField(
+        default=default_profile_pic,
+        upload_to=profile_pics_dir,
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 
 # register our namespace
 app_name = 'users'
@@ -28,3 +31,6 @@ urlpatterns = [
     ),
 ]
 
+# get correct location for media files in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
