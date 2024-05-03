df9055cb54befd66b2e23917e7f377d6e448d8e3
Geert linders <geert.linders@salcom.com>
2024-05-03 17:51:20 +1200

    completed updating user proifle & rescale profile image
    Change-Id: I2e19614975b836d78a9da59861c8b50c7285a307

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 import os
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 profile_pics_dir = 'profile_pics'
 default_profile_pic = os.path.join(profile_pics_dir, 'default.png')
 
 
 class Profile(models.Model):
-    # connect out user model to django's user model
+    # connect our user model to django's user model
     # on delete, delete everything i.e. cascade
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # extend the model with a profile picture
@@ -15,6 +17,17 @@ class Profile(models.Model):
         default=default_profile_pic,
         upload_to=profile_pics_dir,
     )
+
+    def save(self):
+        # first save image
+        super().save()
+        # get current image
+        img = Image.open(self.image.path)
+        # then resize image to save space & reduce load time
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- users/views.py
+++ users/views.py
@@ -12,10 +12,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, (
-                f'The account for {username} has been created.'''
-                ' You are now able to log in.'
-            ))
+            messages.success(
+                request,
+                (
+                    f'The account for {username} has been created.'
+                    ' You are now able to log in.'
+                ),
+            )
             return redirect('users:login')
     else:
         # create an empty form
@@ -29,8 +32,28 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,  # user profile image
+            instance=request.user.profile
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            username = u_form.cleaned_data.get('username')
+            messages.success(
+                request,
+                f'The account for {username} has been updated.',
+            )
+            return redirect('users:profile')
+    else:
+        # populate user form with current user's data
+        u_form = UserUpdateForm(instance=request.user)
+        # populate profile form with current user's profile
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'title': 'Profile',
         'u_form': u_form,
