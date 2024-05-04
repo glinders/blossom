ce243e7647fa8c7f0c1a3414544c6c1eb07cbfbe
Geert linders <geert.linders@salcom.com>
2024-05-05 10:15:53 +1200

    added class based views & post detail page
    Change-Id: I40f4f5fc7c7e29f74ce1271ab3663f81561c8811

--- ccf/urls.py
+++ ccf/urls.py
@@ -6,6 +6,28 @@ from . import views
 app_name = 'ccf'
 
 urlpatterns = [
+    # home page; lists all posts
+    path(
+        '',
+        views.PostListView.as_view(),
+        name='home',
+    ),
+    # page to view single post
+    path(
+        'post/<int:pk>/',
+        views.PostDetailView.as_view(),
+        name='post-detail',
+    ),
+    # about page
+    path(
+        'about/',
+        views.about,
+        name='about',
+    ),
+]
+
+# these contain the function based views
+urlpatterns_unused = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
 ]
--- ccf/views.py
+++ ccf/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
 
 
+# class based view for the home page
+# by default looks for a template of the form:
+#   <app>/<model>_<viewtype>.html
+# for our class PostListView this would be:
+#   ccf/post_list.html
+# we can pass our own name by setting attribute template_name
+class PostListView(ListView):
+    # model that is going to be displayed as a list
+    model = Post
+    template_name = 'ccf/home.html'
+    # specify the name for our list objects that we use in our template
+    # the class based view will by default pass all objects of our model
+    # i.e. Post.objects.all()
+    context_object_name = 'posts'
+    # order our posts by date in reversed order (using a '-') to get the
+    # newest posts first i.e. at the top of the page
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# function based view for the home page
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -9,6 +37,7 @@ def home(request):
     return render(request, 'ccf/home.html', context)
 
 
+# function based view for the about page
 def about(request):
     context = {
         'title': 'About',
